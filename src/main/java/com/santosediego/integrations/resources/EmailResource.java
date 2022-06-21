1d3d2521821361e350c63dcb76a691a6c5cd3e54
Diego Santos <diegoeustaquiodossantos@hotmail.com>
2022-06-21 11:14:02 -0300

    Project created, EmailDTO

---
+++ src/main/java/com/santosediego/integrations/resources/EmailResource.java
@@ -0,0 +1,19 @@
+package com.santosediego.integrations.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.santosediego.integrations.dto.EmailDTO;
+
+@RestController
+@RequestMapping(value = "/emails")
+public class EmailResource {
+
+	@PostMapping
+	public ResponseEntity<Void> send(@RequestBody EmailDTO dto){
+		return ResponseEntity.noContent().build();
+	}
+}
---
+++ src/test/java/com/santosediego/integrations/IntegrationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santosediego.integrations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegrationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
