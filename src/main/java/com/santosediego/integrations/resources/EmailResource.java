506ffaa1e8dfb549a69b2e39954a9d74bfb733be
Diego Santos <diegoeustaquiodossantos@hotmail.com>
2022-06-21 18:03:43 -0300

    Sending email, exceeption handling

--- src/main/java/com/santosediego/integrations/resources/EmailResource.java
+++ src/main/java/com/santosediego/integrations/resources/EmailResource.java
@@ -1,5 +1,6 @@
 package com.santosediego.integrations.resources;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -7,13 +8,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.santosediego.integrations.dto.EmailDTO;
+import com.santosediego.integrations.services.EmailService;
 
 @RestController
 @RequestMapping(value = "/emails")
 public class EmailResource {
 
+	@Autowired
+	private EmailService service;
+
 	@PostMapping
-	public ResponseEntity<Void> send(@RequestBody EmailDTO dto){
+	public ResponseEntity<Void> send(@RequestBody EmailDTO dto) {
+		service.sendEmail(dto);
 		return ResponseEntity.noContent().build();
 	}
 }
---
+++ src/main/java/com/santosediego/integrations/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.santosediego.integrations.resources.exceptions;
+
+import java.time.Instant;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.santosediego.integrations.services.exceptions.EmailException;
+import com.santosediego.integrations.services.exceptions.StandardError;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+	@ExceptionHandler(EmailException.class)
+	public ResponseEntity<StandardError> email(EmailException e, HttpServletRequest request) {
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError err = new StandardError();
+		err.setTimestamp(Instant.now());
+		err.setStatus(status.value());
+		err.setError("Email error");
+		err.setMessage(e.getMessage());
+		err.setPath(request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+}
